aacc5a955c923d012e7435b2e452cdeed3c8348e
samuel-cloud56 <firediamondz782@gmail.com>
2021-05-16 15:41:10 +0100

    first commit

---
+++ my_functions.js
@@ -0,0 +1,34 @@
+let digit = prompt('Enter number 1 - 100')
+let digits = ( digit % 2 == 0)? 'Even Number': 'Odd Number'
+alert(digits)
+
+const minus = () =>{
+    let a = 6
+    let b = 6
+    return a - b
+}
+console.log(minus())
+
+const sqr = () =>{
+    let a = 10
+    let b = 2
+    return a % b
+}
+console.log(sqr())
+
+const Times = () =>{
+    let a = 2
+    let b = 2
+    return a*b
+}
+console.log(Times())
+
+const divd =() =>{
+    let a = 20
+    let b = 5
+    return a/b
+}
+console.log(divd())
+
+let Sam = prompt("Enter Sentences").toUpperCase()
+alert(Sam)
